5a596be1bed6e441439c0156b2dcabae58586450
spiedra <jcspiedra@gmail.com>
2021-06-14 12:48:14 -0600

    ui login completed

--- view/indexView.php
+++ view/indexView.php
@@ -6,12 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="Juan Carlos Sequeira Piedra" />
     <meta name="keywords" content="store, covid, online, clean" />
-    <link rel="stylesheet" href="public/css/style.css"/>
-    <link rel="stylesheet" href="public/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="public/css/style.css" />
     <title>Online Store</title>
 </head>
 
 <body>
-
-<script src="public/js/bootstrap.bundle.min.js"></script>
+    <main class="container-fluid vh-100 d-flex align-items-center justify-content-center page-main">
+        <form class="container bg-warning form__log-in">
+            <div class="container">
+                <h1 class="mt-3 text-center">Log in</h1>
+            </div>
+            <div class="container container--one">
+                <label class="mb-2">User name</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter user name">
+            </div>
+            <div class="container container--two">
+                <label class="mb-2">Password</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Password">
+            </div>
+            <div class="container container--three">
+                <a class="">Sign in</a>
+            </div>
+            <div class="container d-flex justify-content-center container--four">
+                <button type="submit" class="btn btn-primary">Log in</button>
+            </div>
+        </form>
+    </main>
+    </div>
+    <script src="public/js/bootstrap.bundle.min.js"></script>
 </body>
