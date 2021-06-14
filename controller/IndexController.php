809f09c9cf3e618d33c6ff8aab4199ed3a431eb2
spiedra <jcspiedra@gmail.com>
2021-06-13 21:21:57 -0600

    added indexView

---
+++ controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+class IndexController {
+
+    public function __construct() {
+        $this->view = new View();
+    }
+
+    public function showIndexView() {
+        $this->view->show("indexView.php", null);
+    }
+}
+
+?>
---
+++ view/indexView.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Juan Carlos Sequeira Piedra" />
+    <meta name="keywords" content="store, covid, online, clean" />
+    <link rel="stylesheet" href="../public/css/style.css"/>
+    <link rel="stylesheet" href="../public/css/bootstrap.min.css"/>
+    <title>Online Store</title>
+</head>
