03544d937d6e828a99d0e8a8df3bca791a1075a4
spiedra <jcspiedra@gmail.com>
2021-06-13 23:05:20 -0600

    bootstrap added

--- view/indexView.php
+++ view/indexView.php
@@ -3,10 +3,15 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="Juan Carlos Sequeira Piedra" />
     <meta name="keywords" content="store, covid, online, clean" />
-    <link rel="stylesheet" href="../public/css/style.css"/>
-    <link rel="stylesheet" href="../public/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="public/css/style.css"/>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css"/>
     <title>Online Store</title>
 </head>
+
+<body>
+
+<script src="public/js/bootstrap.bundle.min.js"></script>
+</body>
