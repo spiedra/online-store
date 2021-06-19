8a8222a7567843893006881650c0fdc79331a2e2
spiedra <jcspiedra@gmail.com>
2021-06-19 01:02:09 -0600

    updated navbar

--- view/headerAdminView.php
+++ view/headerAdminView.php
@@ -3,7 +3,7 @@
         <div class="mx-2">
             <a href="?controller=Index&action=showIndexView" class="btn btn-primary me-auto"><i class="fas fa-arrow-left"></i></a>
         </div>
-        <div class="container-fluid justify-content-end flex-row">
+        <div class="container-fluid justify-content-end">
             <div class="dropdown d-none d-md-flex d-lg-flex">
                 <button class="btn btn-secondary dropdown-toggle btn-dropdow text-dark" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                     Register
@@ -30,27 +30,31 @@
                     </li>
                 </ul>
             </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse flex-grow-0" id="toggleMobileMenu">
-                <ul class="nav ms-auto text-dark flex-column flex-md-row align-items-end justify-content-evenly">
-                    <li class="nav-item d-flex d-md-none d-lg-none">
-                        <a href="?controller=Admin&action=showAdminRegisterView" class="nav-link text-dark">Register admin</a>
-                    </li>
-                    <li class="nav-item d-flex d-md-none d-lg-none">
-                        <a href="?controller=Product&action=showProductRegisterView" class="nav-link text-dark">Register products</a>
-                    </li>
-                    <li class="nav-item d-flex d-md-none d-lg-none">
-                        <a href="?controller=Category&action=showCategoryRegisterView" class="nav-link text-dark">Create category</a>
-                    </li>
-                    <li class="nav-item d-flex d-md-none d-lg-none">
-                        <a href="?controller=Promotion&action=showPromotionView" class="nav-link text-dark">Create promotions</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-dark">Report</a>
-                    </li>
-                </ul>
+            <div class="d-flex justify-content-end align-items-center flex-column">
+                <div class="container d-flex justify-content-end">
+                <button class="navbar-toggler text-right" type="button" data-bs-toggle="collapse" data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                </div>
+                <div class="collapse navbar-collapse flex-column" id="toggleMobileMenu">
+                    <ul class="nav ms-auto text-dark flex-column flex-md-row align-items-end justify-content-evenly">
+                        <li class="nav-item d-flex d-md-none d-lg-none">
+                            <a href="?controller=Admin&action=showAdminRegisterView" class="nav-link text-dark">Register admin</a>
+                        </li>
+                        <li class="nav-item d-flex d-md-none d-lg-none">
+                            <a href="?controller=Product&action=showProductRegisterView" class="nav-link text-dark">Register products</a>
+                        </li>
+                        <li class="nav-item d-flex d-md-none d-lg-none">
+                            <a href="?controller=Category&action=showCategoryRegisterView" class="nav-link text-dark">Create category</a>
+                        </li>
+                        <li class="nav-item d-flex d-md-none d-lg-none">
+                            <a href="?controller=Promotion&action=showPromotionView" class="nav-link text-dark">Create promotions</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-dark">Report</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </nav>
