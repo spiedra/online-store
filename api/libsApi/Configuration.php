556a91ed7d6dcb164ecd09dc3e9f42f268251f89
spiedra <jcspiedra@gmail.com>
2021-06-28 12:44:20 -0600

    updated configuration.php

--- api/libsApi/Configuration.php
+++ api/libsApi/Configuration.php
@@ -5,12 +5,12 @@ $config = Config::singleton();
 $config->set('controllerFolder', 'controllers/');
 $config->set('modelFolder', 'models/');
 
-// $config->set('dbhost', '163.178.107.10'); 
-// $config->set('dbname', 'B97452_PROYECTO2_IF4101');
-// $config->set('dbuser', 'laboratorios');
-// $config->set('dbpass', 'KmZpo.2796');
+$config->set('dbhost', '163.178.107.10'); 
+$config->set('dbname', 'B97452_PROYECTO2_IF4101');
+$config->set('dbuser', 'laboratorios');
+$config->set('dbpass', 'KmZpo.2796');
 
-$config->set('dbhost', 'localhost'); 
-$config->set('dbname', 'b97452_proyecto2_if4101');
-$config->set('dbuser', 'root');
-$config->set('dbpass', 'piedra');
+// $config->set('dbhost', 'localhost'); 
+// $config->set('dbname', 'b97452_proyecto2_if4101');
+// $config->set('dbuser', 'root');
+// $config->set('dbpass', 'piedra');
